747a57e28ddf4a4a06abf8fd30f8fca632fc1ea5
italolukas01 <italolukas01@gmail.com>
2018-04-17 08:27:53 -0300

    Add files via upload
    equipamento php

---
+++ CODIGO-FONTE/equipamentos.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>SISTEMEQ</title>
+
+    </head>
+
+    <body>
+
+        <div id="wrapper">
+
+            <!-- Navigation -->
+            <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">SISTEMEQ<img src="../img/una-logo.jpg"></a>
+                </div>
+                <!-- Top Menu Items -->
+                <ul class="nav navbar-right top-nav">                
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> Administrador <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li>
+                                <a href="login.php"><i class="fa fa-fw fa-power-off"></i> Sair</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+ 
+            </nav>
+
+            <div id="page-wrapper">
+
+                <div class="container-fluid">
+
+
+                </body>
+
+                </html>
